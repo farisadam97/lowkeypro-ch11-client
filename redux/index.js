c1d7b6bb4461d3f2f38ebe146f7b470ccff6f387
Hany Luqianie <luqianiehany@gmail.com>
2022-02-12 14:42:26 +0700

    finished redux top nav bar

--- redux/index.js
+++ redux/index.js
@@ -1,9 +1,11 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import { userReducer } from './reducers/users'
+import { counterReducer } from './reducers/counters'
 
 const rootReducer = combineReducers({
-    users: userReducer
+    users: userReducer,
+    counters: counterReducer
 })
 
 export const store = createStore(rootReducer, applyMiddleware(thunk))
