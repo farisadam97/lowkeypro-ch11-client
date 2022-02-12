53c5df91c201ceba14389e9c66458edc88428864
ajhosan <alexjhosan19@gmail.com>
2022-02-13 02:38:47 +0700

    message component

--- components/RPSGame/message/Message.component.jsx
+++ components/RPSGame/message/Message.component.jsx
@@ -1,12 +1,11 @@
-import './Message.css';
 
-export const Message = ({ userSelection, message }) => {
+export const Message = ({ data }) => {
     return (
         <>
         {
-                userSelection === "" ? (<p id="vs">VS</p>) : 
-                message === "Player win!" ? (<p className="result">Player Win!</p>) : 
-                message === "Draw!" ? (<p className="resultdraw">Draw!</p>) : 
+                data.userselection === "" ? (<p id="vs">VS</p>) : 
+                data.message === "Player win!" ? (<p className="result">Player Win!</p>) : 
+                data.message === "Draw!" ? (<p className="resultdraw">Draw!</p>) : 
                 (<p className="resultlost">You lost!</p>)
         }
         </>
