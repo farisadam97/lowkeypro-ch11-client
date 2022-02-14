f8022672d963f5c41c0b274b4449d568d90a38af
ajhosan <alexjhosan19@gmail.com>
2022-02-14 13:29:46 +0700

    store redux

--- redux/index.js
+++ redux/index.js
@@ -1,8 +1,10 @@
 import { combineReducers, createStore } from 'redux'
 import { rpsgameReducer } from './reducers/rpsgame.reducer'
+import { statusGameReducer } from './reducers/statusgame.reducer'
 
 const rootReducer = combineReducers({
     rpsGame: rpsgameReducer,
+    statusGame: statusGameReducer
 })
 
 export const store = createStore(rootReducer)
