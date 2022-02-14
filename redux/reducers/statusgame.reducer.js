f4ae1eb62618c4207cd1a51265088dd7b611a9c1
ajhosan <alexjhosan19@gmail.com>
2022-02-14 13:28:07 +0700

    redux status game

---
+++ redux/reducers/statusgame.reducer.js
@@ -0,0 +1,20 @@
+import { ADD_STATUS } from "../constants/statusgame.constatn"
+
+export const initialState = {
+    rpsGame: "",
+    rpsStatusgame: ""        
+}
+
+
+export const statusGameReducer = (state = initialState, action) => {
+    // console.log(action.payload)
+    switch (action.type) {
+        case ADD_STATUS:
+            return {
+                ...state,
+                    ...state = action.payload
+            }
+        default:
+            return state
+    }
+}
