356e46ff48f8a60147ebefcb7d7209cd246704e6
ajhosan <alexjhosan19@gmail.com>
2022-02-15 14:03:30 +0700

    redux navbar

--- redux/index.js
+++ redux/index.js
@@ -4,12 +4,14 @@ import { userReducer } from './reducers/users'
 import { counterReducer } from './reducers/counters'
 import { rpsgameReducer } from './reducers/rpsgame.reducer'
 import { statusGameReducer } from './reducers/statusgame.reducer'
+import { navbarReducer } from './reducers/navbar.reducer'
 
 const rootReducer = combineReducers({
     users: userReducer,
     counters: counterReducer,
     rpsGame: rpsgameReducer,
-    statusGame: statusGameReducer
+    statusGame: statusGameReducer,
+    navbar: navbarReducer
 })
 
 export const store = createStore(rootReducer, applyMiddleware(thunk))
