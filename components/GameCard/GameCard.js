3a3c7553d02f096bb69a281a19d421e0d9d01ecc
Hany Luqianie <luqianiehany@gmail.com>
2022-02-15 17:30:17 +0700

    finished redux dummy games

--- components/GameCard/GameCard.js
+++ components/GameCard/GameCard.js
@@ -10,9 +10,23 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { addStatusGame } from '../../redux/actions/statusgame.action';
 
 const GameCard = (props) => {
+	const dispatch = useDispatch()
     const statusGame = useSelector(store => store.statusGame);
+    const counters = useSelector(state => state.counters);
+	
+	const handlePlayButtonClicked = (e) => {
+		dispatch(
+			addStatusGame({
+				rpsGame: "true",
+				rpsStatusgame: "Game pernah dimainkan sebelumnya"
+			})
+		)
+    }
+	
     return(
         <Grid item xs={12} md={6} lg={4}>
             <Card sx={{ height:'100%', borderRadius:4 }}>
@@ -27,20 +41,25 @@ const GameCard = (props) => {
 						</Alert>
 					)
 				}
-                <Image
-                    src={props.image} alt="" 
-                    width="700"
-                    height="600"
-                />
+                <Link href={props.link}>
+					<a target="_blank" onClick={handlePlayButtonClicked}>
+						<Image
+						src={props.image} alt="" 
+						width="700"
+						height="600"
+						/>
+					</a>
+                </Link>
                 <CardContent sx={{px:3}}>
                     <Grid container spacing={2}>
                         <Grid item xs={7}>
                             <Typography gutterBottom variant="h5" component="div">
-                                {props.title}
+                                Game {props.gamenumber}:
+								<br/>{props.title}
                             </Typography>
                         </Grid>
                         <Grid item xs={5}>
-                             <p style={{ fontFamily: 'Roboto,Helvetica,Arial,sans-serif', fontSize: '0.875rem', lineHeight: '1.75', textTransform: 'uppercase', textDecoration: 'none', padding: '4px 15px', textAlign: "right" }}><span style={{ backgroundColor: '#55daab', color: 'white', borderRadius: '3px', padding: '2px 6px',}}>Score: 50</span></p> 
+                             <p style={{ fontFamily: 'Roboto,Helvetica,Arial,sans-serif', fontSize: '0.875rem', lineHeight: '1.75', textTransform: 'uppercase', textDecoration: 'none', padding: '4px 15px', textAlign: "right" }}><span style={{ backgroundColor: '#55daab', color: 'white', borderRadius: '3px', padding: '2px 6px',}}>Score: {counters.value}</span></p> 
                         </Grid>
                     </Grid>
                     <Typography variant="body2" color="text.secondary">
@@ -50,7 +69,7 @@ const GameCard = (props) => {
                 <CardActions sx={{px:3}}>
                     <Button variant="contained" sx={{width:"100%" , my:2}}>
                         <Link style={{ textDecoration: 'none', color: '#fff' }} href={props.link}>
-                           <a>Play Now!</a>
+                           <a target="_blank" onClick={handlePlayButtonClicked}>Play Now!</a>
                         </Link>
                     </Button>
                 </CardActions>
--- pages/game-list.js
+++ pages/game-list.js
@@ -5,9 +5,14 @@ import GameCard from '../components/GameCard/GameCard';
 import styles from '../styles/gamelist.module.css'
 import ResponsiveAppBar from '../components/navbar/Navbar.component';
 import imageRPS from '../public/images/RPSgame.jpeg';
-
-
-
+import imagegame2 from '../public/images/game2.jpg';
+import imagegame3 from '../public/images/game3.jpg';
+import imagegame4 from '../public/images/game4.jpg';
+import imagegame5 from '../public/images/game5.jpg';
+import imagegame6 from '../public/images/game6.jpg';
+import imagegame7 from '../public/images/game7.jpg';
+import imagegame8 from '../public/images/game8.jpg';
+import imagegame9 from '../public/images/game9.jpg';
 
 const GameList = () => {
 
@@ -17,6 +22,54 @@ const GameList = () => {
             description:"A well known traditional games from entire world that could ruin your friendship with your bestfriend",
             image: imageRPS,
             link:"/rps-game"
+        },
+		{
+            title: "Female Assassin",
+            description:"A well known first person shooting game with fast paced action, can play multiplayer online with your friends",
+            image: imagegame2,
+            link:"https://google.com"
+        },
+		{
+            title: "I Am Fastest",
+            description:"Challenge your friends in this fun racing game, who is the fastest among all?",
+            image: imagegame3,
+            link:"https://google.com"
+        },
+		{
+            title: "Zombie Outbreak",
+            description:"Kill the zombies and save the world",
+            image: imagegame4,
+            link:"https://google.com"
+        },
+		{
+            title: "Super Mario Bros.",
+            description:"A classic and exciting nintendo game",
+            image: imagegame5,
+            link:"https://google.com"
+        },
+		{
+            title: "Alien Killer",
+            description:"Conquer the universe and smash the aliens in this new game",
+            image: imagegame6,
+            link:"https://google.com"
+        },
+		{
+            title: "The Warships",
+            description:"Battle with your friends in world war game and conquer the world",
+            image: imagegame7,
+            link:"https://google.com"
+        },
+		{
+            title: "Dragons & Magic",
+            description:"Slay the dragon in this magical world, save the kingdom and the king",
+            image: imagegame8,
+            link:"https://google.com"
+        },
+		{
+            title: "The Sims",
+            description:"Welcome to the world of the sims, build your house and make it pretty",
+            image: imagegame9,
+            link:"https://google.com"
         }
     ]
     
@@ -32,6 +85,7 @@ const GameList = () => {
                         gameListItem.map((item,index) => (
                             <GameCard
                                 key={index} 
+								gamenumber={index+1} 
                                 title={item.title} 
                                 description={item.description}
                                 image={item.image}
