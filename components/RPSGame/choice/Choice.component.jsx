2f79b3213d8b6026eb5d37360eecbfd3b9066a02
ajhosan <alexjhosan19@gmail.com>
2022-03-08 13:46:50 +0700

    update metode get image

--- components/RPSGame/choice/Choice.component.jsx
+++ components/RPSGame/choice/Choice.component.jsx
@@ -3,8 +3,8 @@ import {settings} from '../config/game';
 import Image from 'next/image';
 
 import Typography from '@mui/material/Typography';
-import imgfacebook from '../../../public/images/logo-facebook.png';
-import imgtwitter from '../../../public/images/logo-twitter.png';
+// import imgfacebook from '../../../public/images/logo-facebook.png';
+// import imgtwitter from '../../../public/images/logo-twitter.png';
 
 export const Choice = ({ value, id, className, choiceIcon, onClick }) => {
     return (
@@ -23,7 +23,6 @@ export const Choice = ({ value, id, className, choiceIcon, onClick }) => {
   };
 
 export const Selection = ({children, trophyIcon, data}) => {
-    // console.log(data)
     return(
         <>
         {data.userscore < settings.winTarget ? (
@@ -57,7 +56,7 @@ export const Selection = ({children, trophyIcon, data}) => {
 			<div style={{textAlign:"center"}}>
 				<a href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Flowkeypro.netlify.app%2F&quote=I won this Rock Paper Scissor Game and you should try it too" target="_new" className="iconshare iconsharefb">
                     <Image 
-                        src={imgfacebook}
+                        src="/images/logo-facebook.png"
                         alt="Facbook"
                         height={15}
                         width={19}
@@ -65,7 +64,7 @@ export const Selection = ({children, trophyIcon, data}) => {
                 </a>
 				<a href="https://twitter.com/intent/tweet?text=I won this Rock Paper Scissor Game and you should try it too https://lowkeypro.netlify.app/" target="_new" className="iconshare iconsharett">
                     <Image 
-                        src={imgtwitter}
+                        src="/images/logo-twitter.png"
                         alt="Twitter"
                         height={15}
                         width={19}
@@ -114,7 +113,7 @@ export const SelectionComputer = ({children, trophyIcon, data}) => {
                     <div style={{textAlign:"center"}}>
                         <a href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Flowkeypro.netlify.app%2F&quote=Check out this cool Rock Paper Scissor Game" target="_new" className="iconshare iconsharefb">
                             <Image 
-                                src={imgfacebook}
+                                src="/images/logo-facebook.png"
                                 alt="Facbook"
                                 height={15}
                                 width={19}
@@ -122,7 +121,7 @@ export const SelectionComputer = ({children, trophyIcon, data}) => {
                         </a>
                         <a href="https://twitter.com/intent/tweet?text=Check out this cool Rock Paper Scissor Game https://lowkeypro.netlify.app/" target="_new" className="iconshare iconsharett">
                             <Image 
-                                src={imgtwitter}
+                                src="/images/logo-twitter.png"
                                 alt="Twitter"
                                 height={15}
                                 width={19}
@@ -138,7 +137,7 @@ export const SelectionComputer = ({children, trophyIcon, data}) => {
 
 export const ChoiceComputer = ({ id, className, imgbatu, imgkertas, imggunting, activeimg, trophyIcon, data}) => {
     return (
-        <>
+        <div>
             {data.pcscore < settings.winTarget ? 
             (
                 data.userselection === "" ?  (
@@ -265,12 +264,11 @@ export const ChoiceComputer = ({ id, className, imgbatu, imgkertas, imggunting, 
                             src={imggunting}
                             alt="trophy"
                     />
-                    {/* <img src={imggunting} alt="trophy" /> */}
                     <h3 style={{textAlign:"center"}}>Victory!</h3>
                 </>
             )
             }
-        </>
+        </div>
       
     );
   };
